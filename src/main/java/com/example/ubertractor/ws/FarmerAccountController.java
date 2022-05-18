a6ae7c3a8c2a5db1ce40b00e1742aacb0b5bac59
Halit Mancar <halitmancar@gmail.com>
2022-05-18 14:59:59 +0300

    Return Type düzenlemesi
    String olarak dönülen response DataResult olarak değiştirildi. Artık JSON tipinde dönüyor.

--- src/main/java/com/example/ubertractor/ws/FarmerAccountController.java
+++ src/main/java/com/example/ubertractor/ws/FarmerAccountController.java
@@ -3,7 +3,10 @@ package com.example.ubertractor.ws;
 import com.example.ubertractor.business.abstracts.account.FarmerAccountService;
 import com.example.ubertractor.business.requests.account.CreateFarmerAccountRequest;
 import com.example.ubertractor.business.requests.account.LoginRequest;
+import com.example.ubertractor.core.utilities.results.DataResult;
+import com.example.ubertractor.core.utilities.results.ErrorDataResult;
 import com.example.ubertractor.core.utilities.results.Result;
+import com.example.ubertractor.core.utilities.results.SuccessDataResult;
 import com.example.ubertractor.entities.account.FarmerAccount;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -30,11 +33,12 @@ public class FarmerAccountController {
         return this.farmerAccountService.login(loginRequest);
     }
     @GetMapping("/getNameByPN")
-    public String getNameByPN(@RequestParam String phoneNumber){
+    public DataResult<String> getNameByPN(@RequestParam String phoneNumber){
         if (this.farmerAccountService.getAccByPN(phoneNumber).getData() == null) {
-            return "Bu telefon numarası ile kayıtlı bir kullanıcı bulunamadı!";
+            return new ErrorDataResult<String>(null, "Bu telefon numarası ile kayıtlı bir kullanıcı bulunamadı!");
         }
-        return this.farmerAccountService.getAccByPN(phoneNumber).getData().getFullName();
+        String name = this.farmerAccountService.getAccByPN(phoneNumber).getData().getFullName();
+        return new SuccessDataResult<String>(name);
     }
     @GetMapping("/getAll")
     public List<FarmerAccount> getAll(){
